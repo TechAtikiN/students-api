f63310d7862c1ee863bd52a2fd8f978e0fef89fa
techatikin <nikitakhabya03@gmail.com>
2025-04-23 20:59:44 +0530

    chore: graceful shutdown

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,7 +1,63 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/techatikin/students-api/internal/config"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	// load config
+	cfg := config.MustLoad()
+
+	// database setup
+
+	// router setup
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World"))
+	})
+
+	// setup server
+	server := http.Server{
+		Addr:    cfg.Addr,
+		Handler: router,
+	}
+
+	slog.Info("Server Started %s", slog.String("address", cfg.Addr))
+
+	done := make(chan os.Signal, 1)
+
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Fatal("Error starting server:", err)
+		}
+	}()
+
+	<-done
+
+	slog.Info("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+
+	defer cancel()
+
+	err := server.Shutdown(ctx)
+	if err != nil {
+		log.Fatal("Error shutting down server:", slog.String("error", err.Error()))
+	}
+
+	slog.Info("Server stopped")
+	slog.Info("Bye!")
 }
