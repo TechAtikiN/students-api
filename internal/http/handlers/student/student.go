cf7301fe53e87e9e7d3d9da969d875d2ae2dcebe
techatikin <nikitakhabya03@gmail.com>
2025-04-24 17:34:55 +0530

    feat: add get all students api

--- internal/http/handlers/student/student.go
+++ internal/http/handlers/student/student.go
@@ -76,3 +76,18 @@ func GetById(storage storage.Storage) http.HandlerFunc {
 		response.WriteJSON(w, http.StatusOK, student)
 	}
 }
+
+func GetList(storage storage.Storage) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		slog.Info("getting all students")
+
+		students, err := storage.GetStudents()
+		if err != nil {
+			response.WriteJSON(w, http.StatusInternalServerError, response.GeneralError(err))
+			return
+		}
+
+		response.WriteJSON(w, http.StatusOK, students)
+	}
+}
